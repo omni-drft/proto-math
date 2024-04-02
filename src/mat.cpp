426143e3a718af5ac4258a36dfdf71945df9762b
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-04-02 19:10:37 +0100

    Testing if memory alocation is working properly with the constructor

--- src/mat.cpp
+++ src/mat.cpp
@@ -1,1 +1,31 @@
 #include "mat.hpp"
+
+template<typename T>
+pm::Mat<T>::Mat(size_t n, size_t m)
+	: n(n), m(m)
+{
+	mat = new T*[n];
+	for (size_t i = 0; i < n; i++)
+		mat[i] = new T[m];
+	for (size_t i = 0; i < n; i++)
+		for (size_t j = 0; j < m; j++)
+			mat[i][j] = 0;
+}
+
+template<typename T>
+pm::Mat<T>::~Mat()
+{
+	for (size_t i = 0; i < n; i++)
+		delete[] mat[i];
+	delete[] mat;
+}
+
+// Explicit instantiation
+template class pm::Mat<int>;
+template class pm::Mat<float>;
+template class pm::Mat<double>;
+template class pm::Mat<long>;
+template class pm::Mat<long long>;
+template class pm::Mat<unsigned int>;
+template class pm::Mat<unsigned long>;
+template class pm::Mat<unsigned long long>;
--- tests/Main.cpp
+++ tests/Main.cpp
@@ -4,26 +4,7 @@
 
 int main() 
 {
-	double a[3] = {1, 2, 3};
-	pm::Vec v1(a, 3);
-
-	pm::Vec v2{ v1.getNormalized() };
-	v2.print();
-
-	double b[3] = {4, 5, 6};
-	pm::Vec v3(b, 3);
-
-	pm::Vec v4{ pm::vector::crossProduct(v1, v3) };
-	v4.print();
-
-	pm::Vec v5{ pm::vector::projection(v1, v3) };
-	v5.print();
-
-	double angle = pm::vector::angle(v1, v3, PM_DEGREES);
-	std::cout << "Angle: " << angle << std::endl;
-
-	bool ortho = pm::vector::orthogonal(v1, v3);
-	std::cout << "Orthogonal: " << ortho << std::endl;
+	pm::Mat<int> a(3, 3);
 
 	return 0;
 }
