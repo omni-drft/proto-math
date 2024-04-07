61fcf6d5dab8da437437de59d6ecaf6a67ee5e11
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-04-07 23:38:39 +0200

    Finishing the initial version of vector section in library

--- include/vec.h
+++ include/vec.h
@@ -2,6 +2,12 @@
 #define VEC_H
 
 #include <math.h>
+#include <stdio.h>
+
+enum pmVectorEnum {
+	pmCompact,
+	pmFull
+};
 
 // Proto Math structure for holding 3-component vectors and 
 // perform calculations on them. At the moment only floating 
@@ -21,13 +27,20 @@ unsigned int pmVec3Divide(struct pmVector3*, float);
 // A function that takes two vectors as first two arguments 
 // and calculates the cross product of those two vectors.
 // The result is returned as struct.
-struct pmVector3 pmVec3CrossProduct(struct pmVector3*, struct pmVector3*);
+struct pmVector3 pmVec3CrossProduct(const struct pmVector3*, const struct pmVector3*);
+
+// A function that calculates the dot product of two given
+// vectors and returns it as float value.
+float pmVec3DotProduct(const struct pmVector3*, const struct pmVector3*);
 
 // Functions that are an alternative form of accessing coordinates.
-float x(struct pmVector3*);
-float y(struct pmVector3*);
-float z(struct pmVector3*);
+float x(const struct pmVector3*);
+float y(const struct pmVector3*);
+float z(const struct pmVector3*);
 
+
+// A function that calculates magnitude (length) of given
+// vector and returns it as float value.
 float pmVec3Length(struct pmVector3*);
 
 // A function that normalizes vector under given address.\
@@ -40,6 +53,10 @@ unsigned int pmVec3NormalizeInPlace(struct pmVector3*);
 // It doesn't modify the given vector.
 struct pmVector3 pmVec3NormalizeCopy(struct pmVector3);
 
-
+// A function that prints the vector to the console using stdio.h.
+// It has two modes:
+//	- pmCompact - a mode when the float values are displayed in short form
+//  - pmFull - a mode when the float values are displayed in their full form
+void pmVec3Print(struct pmVector3*, enum pmVectorEnum mode);
 
 #endif // VEC_H!
--- src/vec.c
+++ src/vec.c
@@ -25,7 +25,7 @@ unsigned int pmVec3Divide(struct pmVector3* vector, float scalar)
 	return 0;
 }
 
-struct pmVector3 pmVec3CrossProduct( struct pmVector3* sourceVec1, struct pmVector3* sourceVec2)
+struct pmVector3 pmVec3CrossProduct( const struct pmVector3* sourceVec1, const struct pmVector3* sourceVec2)
 {
 
 	struct pmVector3 destinationVec;
@@ -48,51 +48,87 @@ struct pmVector3 pmVec3CrossProduct( struct pmVector3* sourceVec1, struct pmVect
 	return destinationVec;
 }
 
-float x(struct pmVector3* vector)
+float pmVec3DotProduct(const struct pmVector3* vector1, const struct pmVector3* vector2)
 {
+	return vector1->components[0] * vector2->components[0] + 
+		vector1->components[1] * vector2->components[1] + 
+		vector1->components[2] * vector2->components[2];
+}
+
+float x(const struct pmVector3* vector)
+{
+	// get first component
 	return vector->components[0];
 }
 
-float y(struct pmVector3* vector)
+float y(const struct pmVector3* vector)
 {
+	// get middle component
 	return vector->components[1];
 }
 
-float z(struct pmVector3* vector)
+float z(const struct pmVector3* vector)
 {
+	// get last component
 	return vector->components[2];
 }
 
 float pmVec3Length(struct pmVector3* vector)
 {
 	float sum = 0;
+	// add all squared components to the sum
 	for (int i = 0; i < 3; i++)
 		sum += vector->components[i] * vector->components[i];
 
+	// return square root of sum
 	return sqrt(sum);
 }
 
-unsigned int normalizeInPlace(struct pmVector3* vector)
+unsigned int pmVec3NormalizeInPlace(struct pmVector3* vector)
 {
-	float length = pmVec3Length(vector);
+	// get length of the given vector
+	float length = pmVec3Length(vector); 
 
+	// if length is 0 then return NULL and print 
+	// the error about trying to divide by 0 
 	if (!length)
 	{
 		printf("ProtoMath Error!: Trying to divide vector by 0!\n");
 		return 1;
 	}
 
+	// divide every component by vector length
 	for (int i = 0; i < 3; i++)
 		vector->components[i] /= length;
 	return 0;
 
 }
 
-struct pmVector3 normalizeCopy(struct pmVector3 vector)
+struct pmVector3 pmVec3NormalizeCopy(struct pmVector3 vector)
 {
+	// get length of the given vector
 	float length = pmVec3Length(&vector);
 
+	// if length is 0 then return NULL and print 
+	// the error about trying to divide by 0
+	if (!length)
+	{
+		printf("ProtoMath Error!: Trying to divide vector by 0!\n");
+		return;
+	}
+
+	// divide every component by the length of vector
 	for (int i = 0; i < 3; i++)
 		vector.components[i] /= length;
 	return vector;
+}
+
+void pmVec3Print(struct pmVector3* vector, enum pmVectorEnum mode)
+{
+	// if the mode is set to pmCompact
+	if (!mode)
+		printf("[ %.2f, %.2f, %.2f ]", vector->components[0], vector->components[1], vector->components[2]);
+	// if the mode is set to pmFull
+	else 
+		printf("[ %f, %f, %f ]", vector->components[0], vector->components[1], vector->components[2]);
 }
