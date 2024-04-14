4a4b934ab08ee972e1ce1c0098aad0b78fb4d6c1
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-04-14 19:20:17 +0200

    Finishing the vec4 functions

--- include/pmVec4.h
+++ include/pmVec4.h
@@ -31,6 +31,45 @@ float y(const struct pmVector4*);
 float z(const struct pmVector4*);
 float w(const struct pmVector4*);
 
+// A function that calculates vector length
 float pmVec4Length(const struct pmVector4*);
+
+// A function that normalizes vector under given address.
+// WARNING!: It modifies the given vector
+// Returns an error code.
+unsigned int pmVec4NormalizeInPlace(struct pmVector4*);
+
+// A function that creates copy of given vector.
+// It normalizes it and returns.
+// It doesn't modify the given vector.
+struct pmVector4 pmVec4NormalizeCopy(struct pmVector4);
+
+// A function that prints the vector to the console using stdio.h.
+// It has two modes:
+//	- pmCompact - a mode when the float values are displayed in short form
+//  - pmFull - a mode when the float values are displayed in their full form
+void pmVec4Print(const struct pmVector4*, enum pmVectorEnum mode);
+
+// A function that calculates angle between two given vectors
+// and returns result as a float value.
+// The result depend on the mode provided by user.
+// Two supported modes are:
+//   - pmRadians
+//	 - pmDegrees
+// If the mode is set to pmRadians the result is angle in radians
+// If the mode is set to pmDegrees the result is angle in degrees
+float pmVec4Angle(const struct pmVector4*, const struct pmVector4*, enum pmVectorEnum mode);
+
+// A function that checks if two given vectors are parallel to each other.
+// Returns:
+//	1 if they are parallel
+//  0 if they are not parallel
+unsigned int pmVec4IsParallel(const struct pmVector4*, const struct pmVector4*);
+
+// A function that checks if two given vectors are perpendicular to each other.
+// Returns:
+//  1 if they are perpendicular
+//  0 if they are not perpendicular
+unsigned int pmVec4IsPerpendicular(const struct pmVector4*, const struct pmVector4*);
 
 #endif // !PMVEC4_H
--- src/pmVec4.c
+++ src/pmVec4.c
@@ -64,3 +64,69 @@ float pmVec4Length(const struct pmVector4* v)
 	return sqrt(sum);
 }
 
+unsigned int pmVec4NormalizeInPlace(struct pmVector4* v)
+{
+	float length = pmVec4Length(v);
+
+	if (!length)
+	{
+		printf("ProtoMath Error!: Trying to divide vector by 0!\n");
+		return 1;
+	}
+
+	for (int i = 0; i < 4; i++)
+		v->components[i] /= length;
+	return 0;
+}
+
+struct pmVector4 pmVec4NormalizeCopy(struct pmVector4 v)
+{
+	float length = pmVec4Length(&v);
+	if (!length)
+	{
+		printf("ProtoMath Error!: Trying to divide vector by 0!\n");
+		return;
+	}
+
+	for (int i = 0; i < 4; i++)
+		v.components[i] /= length;
+	return v;
+}
+
+void pmVec4Print(const struct pmVector4* v, enum pmVectorEnum mode)
+{
+	// if the mode is set to pmCompact
+	if (mode == pmCompact)
+		printf("[ %.2f, %.2f, %.2f, %.2f ]", v->components[0], v->components[1], v->components[2], v->components[3]);
+	// if the mode is set to pmFull
+	else if (mode == pmFull)
+		printf("[ %f, %f, %f, %f ]", v->components[0], v->components[1], v->components[2], v->components[3]);
+}
+
+float pmVec4Angle(const struct pmVector4* v1, const struct pmVector4* v2, enum pmVectorEnum mode)
+{
+	float angle = acos(pmVec4DotProduct(v1, v2) / (pmVec4Length(v1) * pmVec4Length(v1)));
+	if (mode == pmRadians)
+		return angle;
+	else if (mode == pmDegrees)
+		return angle * (180.0 / PM_PI);
+	else
+	{
+		printf("ProtoMath Error!: Wrong print mode.\n");
+		return;
+	}
+}
+
+unsigned int pmVec4IsParallel(const struct pmVector4* v1, const struct pmVector4* v2)
+{
+	if (pmVec4DotProduct(v1, v2) == pmVec4Length(v1) * pmVec4Length(v2))
+		return 1;
+	return 0;
+}
+
+unsigned int pmVec4IsPerpendicular(const struct pmVector4* v1, const struct pmVector4* v2)
+{
+	if (!pmVec4DotProduct(v1, v2))
+		return 1;
+	return 0;
+}
